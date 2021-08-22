52adb36c7e1541402bf3dacb9d7374927d599764
trishan <trishansaha43@gmail.com>
2021-08-22 10:47:29 +0600

    replace method

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const car = {
-  name: 'Bugatti',
-  model: 500,
-  kg: '100km',
-  height: '1,204 mm (47.4 in)',
-  width: '1,998 mm (78.7 in)',
-  description: `
-  The CHIRON is the fastest, most powerful, and exclusive production super sports car in BUGATTI's history. Its sophisticated design, innovative technology, and iconic, performance-oriented form make it a unique masterpiece of art, form and technique, that pushes boundaries beyond imagination.`,
-  start: function () {
-    console.log(
-      `the car name is ${this.name} and also model number is ${this.model}`
-    )
-    console.log('bugatti car will be start soon')
-  },
-  end: function () {
-    console.log('will be end')
-  }
-}
+// const car = {
+//   name: 'Bugatti',
+//   model: 500,
+//   kg: '100km',
+//   height: '1,204 mm (47.4 in)',
+//   width: '1,998 mm (78.7 in)',
+//   description: `
+//   The CHIRON is the fastest, most powerful, and exclusive production super sports car in BUGATTI's history. Its sophisticated design, innovative technology, and iconic, performance-oriented form make it a unique masterpiece of art, form and technique, that pushes boundaries beyond imagination.`,
+//   start: function () {
+//     console.log(
+//       `the car name is ${this.name} and also model number is ${this.model}`
+//     )
+//     console.log('bugatti car will be start soon')
+//   },
+//   end: function () {
+//     console.log('will be end')
+//   }
+// }
 
-alert(car[name])
+// alert(car[name])
 
-car.start()
+// car.start()
 
-function sayHi () {
-  console.log(name)
-  console.log(age)
-  var name = 'Javascript'
-  let age = 21
-}
+// function sayHi () {
+//   console.log(name)
+//   console.log(age)
+//   var name = 'Javascript'
+//   let age = 21
+// }
 
-sayHi()
+// sayHi()
+
+// replace method
+const name = 'My Name is Trishan'
+
+console.log(name.replace('Trishan', 'Sakira'))
