b95695ae522bbeed32b137033275686670a58aa0
trishan <trishansaha43@gmail.com>
2021-08-25 22:18:26 +0600

    makepair

--- index.js
+++ index.js
@@ -1,68 +1,39 @@
-// const car = {
-//   name: 'Bugatti',
-//   model: 500,
-//   kg: '100km',
-//   height: '1,204 mm (47.4 in)',
-//   width: '1,998 mm (78.7 in)',
-//   description: `
-//   The CHIRON is the fastest, most powerful, and exclusive production super sports car in BUGATTI's history. Its sophisticated design, innovative technology, and iconic, performance-oriented form make it a unique masterpiece of art, form and technique, that pushes boundaries beyond imagination.`,
-//   start: function () {
-//     console.log(
-//       `the car name is ${this.name} and also model number is ${this.model}`
-//     )
-//     console.log('bugatti car will be start soon')
-//   },
-//   end: function () {
-//     console.log('will be end')
-//   }
-// }
+// problem solving on Array ------- 01
+const getFirstElement = num => {
+  return num[0]
+}
 
-// alert(car[name])
+// Write a function that takes the base and height of a triangle and return its area. ---- 2
 
-// car.start()
+const triArea = (base, height) => {
+  return base * height
+}
 
-// function sayHi () {
-//   console.log(name)
-//   console.log(age)
-//   var name = 'Javascript'
-//   let age = 21
-// }
+const cubes = a => {
+  return a ** 3
+}
 
-// sayHi()
+// Write a function that converts hours into seconds.
 
-// replace method
-// const name = 'My Name is Trishan'
+const howManySeconds = hours => {
+  return 60 * 60 * hours
+}
 
-// console.log(name.replace('Trishan', 'Sakira'))
+// Is the Number Less than or Equal to Zero?
+const lessThanOrEqualToZero = num => {
+  return num === 0 || num < 0 ? true : false
+}
 
-// javascript string search
-// const str = 'I love Javascript'
+// age calculate
+const calcAge = age => {
+  const yearInDay = 365
+  return age * 365
+}
 
-// if (str.indexOf('Javascript') > -1) {
-//   console.log('Success')
-// } else {
-//   console.log('Not Success')
-// }
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
 
-// const inputValue = 'mouse'
-// const products = ['computer', 'keyboard', 'mouse', 'monitor']
-// if (products.indexOf(inputValue) > -1) {
-//   console.log('result success')
-// } else {
-//   console.log('result not success')
-// }
+const makePair = (num1, num2) => {
+  return [num1, num2]
+}
 
-// const text = 'Hello Bangladesh'
-// // const result = text.includes('llo')
-// const result = text.endsWith('Bangladesh')
-
-// console.log(result)
-
-// Numbers Data Type **************
-const num = 12.1265
-
-console.log(num.valueOf())
-console.log(num.toFixed(2))
-console.log(num.toExponential(2))
-console.log(typeof num.toString())
-console.log((123).toString())
+console.log(makePair(1, 2))
