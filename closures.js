b22152fafac681017d056d1bc4735c466cb85e68
trishan <trishansaha43@gmail.com>
2021-08-31 01:23:04 +0600

    git closure explain with bankAccount

--- closures.js
+++ closures.js
@@ -1,6 +1,27 @@
-var num1 = 2
-var num2 = 3
 var sum = function () {
   return num1 + num2
 }
-console.dir(sum)
+
+var num1 = 2
+
+var doubleFunc = function () {
+  var num2 = 3
+  return function () {
+    return num1 + num2
+  }
+}
+
+const doubleVar = doubleFunc()
+// console.dir(doubleVar)
+
+// Example - 2
+function bankAccount (initialeBalance) {
+  var balance = initialeBalance
+  return function () {
+    return balance
+  }
+}
+
+const account = bankAccount(100000)
+
+// console.log(account())
