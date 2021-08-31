a9e535b82af85d5fa4a85333a2940bc80caf343d
trishan <trishansaha43@gmail.com>
2021-08-31 21:51:11 +0600

    hoisint test into function

--- hoisting.js
+++ hoisting.js
@@ -5,5 +5,24 @@
 // console.log(a)
 
 // when declare a below console it will return undefine
-console.log(a)
-var a = 'Bangladesh'
+// console.log(a)
+// var a = 'Bangladesh'
+
+// hoisting the other thing
+// var a
+// // console.log(a)
+
+// a = 'Bangladesh'
+// // console.log(a)
+
+let LANGUAGE = 'Java'
+let language = 'Javascript'
+
+function getLanguage () {
+  if (!language) {
+    let language = LANGUAGE
+  }
+  return language
+}
+
+// console.log(getLanguage()) // output is javascript
