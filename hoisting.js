413e60f4f13646f5c6a34b8e61aeb64d05a48d03
trishan <trishansaha43@gmail.com>
2021-08-31 21:28:09 +0600

    hoisting declare and describe undefined error

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+// console.log(a) // hoisting.js:1 Uncaught ReferenceError: a is not defined
+
+// but when declare a variable and assign value into a
+// var a = 'Bangladesh'
+// console.log(a)
+
+// when declare a below console it will return undefine
+console.log(a)
+var a = 'Bangladesh'
