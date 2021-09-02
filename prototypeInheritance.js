2af7c6367a854757e4455cb539ab36248a6c258e
trishan <trishansaha43@gmail.com>
2021-09-02 15:10:41 +0600

    transfer function to prototype

--- prototypeInheritance.js
+++ prototypeInheritance.js
@@ -1,8 +1,24 @@
 // tody we are describe prototype inheritance
-function Person (name, age) {
-  this.name = name
-  this.age = age
+
+const personMethod = {
+  eat: function () {
+    console.log(`${this.name} is eating`)
+  },
+  sleep: function () {
+    console.log(`${this.name} is playing`)
+  }
 }
 
-const sakib = new Person('sakib', 44)
-console.log(sakib)
+function Person (name, age) {
+  const person = Object.create(personMethod)
+
+  person.name = name
+  person.age = age
+
+  return person
+}
+
+const sakib = Person('sakib', 35)
+const tamim = Person('tamim', 38)
+console.log(sakib.sleep())
+console.log(tamim.sleep())
