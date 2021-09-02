8ab3de9e4757917550d555385615a5f00ba82804
trishan <trishansaha43@gmail.com>
2021-09-02 15:16:27 +0600

    crete prototype example two

--- prototypeInheritance.js
+++ prototypeInheritance.js
@@ -1,6 +1,64 @@
 // tody we are describe prototype inheritance
 
-const personMethod = {
+// const personMethod = {
+//   eat: function () {
+//     console.log(`${this.name} is eating`)
+//   },
+//   sleep: function () {
+//     console.log(`${this.name} is playing`)
+//   }
+// }
+
+// function Person (name, age) {
+//   const person = Object.create(personMethod)
+
+//   person.name = name
+//   person.age = age
+
+//   return person
+// }
+
+// const sakib = Person('sakib', 35)
+// const tamim = Person('tamim', 38)
+// console.log(sakib.sleep())
+// console.log(tamim.sleep())
+
+// Example - 2
+
+// function Person (name, age) {
+//   //   const person = Object.create(personMethod)
+
+//   this.name = name
+//   this.age = age
+
+//   //   return person
+// }
+
+// Person.prototype = {
+//   eat: function () {
+//     console.log(`${this.name} is eating`)
+//   },
+//   sleep: function () {
+//     console.log(`${this.name} is playing`)
+//   }
+// }
+
+// const sakib = new Person('sakib', 35)
+// const tamim = new Person('tamim', 38)
+// console.log(sakib.eat())
+// console.log(tamim.eat())
+
+// example - 3
+function Person (name, age) {
+  //   const person = Object.create(personMethod)
+
+  this.name = name
+  this.age = age
+
+  //   return person
+}
+
+Person.prototype = {
   eat: function () {
     console.log(`${this.name} is eating`)
   },
@@ -9,16 +67,7 @@ const personMethod = {
   }
 }
 
-function Person (name, age) {
-  const person = Object.create(personMethod)
-
-  person.name = name
-  person.age = age
-
-  return person
-}
-
-const sakib = Person('sakib', 35)
-const tamim = Person('tamim', 38)
-console.log(sakib.sleep())
-console.log(tamim.sleep())
+const sakib = new Person('sakib', 35)
+const tamim = new Person('tamim', 38)
+console.log(sakib.eat())
+console.log(tamim.eat())
