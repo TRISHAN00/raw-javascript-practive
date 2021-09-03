091613811da48ca8ed9e86426a59c31daccf3e92
trishan <trishansaha43@gmail.com>
2021-09-04 00:47:21 +0600

    using higher order function without for loop

--- higherOrder.js
+++ higherOrder.js
@@ -1,3 +1,11 @@
+/*
+ *
+ * Title: Higher Order Function
+ * Description: What is higher order function how it works
+ * Author: Trishan Saha
+ * Date: 09-Set-2021
+ *
+ */
 // higher order function
 
 // test function is a object with an example
@@ -11,14 +19,43 @@
 // console.log(hello.firstName)
 
 // example - 2
-const myfunc = function () {
-  console.log('My function')
-}
-console.dir(myfunc)
+// const myfunc = function () {
+//   console.log('My function')
+// }
+// console.dir(myfunc)
 
-// it i create a flat object let see what is look like
-const person = {
-  name: 'sakib',
-  age: '34'
-}
-console.log(person)
+// // it i create a flat object let see what is look like
+// const person = {
+//   name: 'sakib',
+//   age: '34'
+// }
+// console.log(person)
+
+// Example - 3
+// const student = () => {
+//   return function () {
+//     return function () {
+//       //   console.log('Hello Students')
+//     }
+//   }
+// }
+// student()()()
+
+// Example - 4 without higher order function
+// make a array
+// const numbers = [1, 2, 3, 4]
+// const result = []
+
+// for (let i = 0; i < numbers.length; i++) {
+//   result.push(numbers[i] * 2)
+// }
+// console.log(result)
+
+// Example - 5 usgin higher order function
+// make a array
+const numbers = [1, 2, 3, 4]
+const result = numbers.map(number => {
+  return number * 2
+})
+
+console.log(result)
