01288adde378fa5997d6db77d6b4fabd27ac3ceb
trishan <trishansaha43@gmail.com>
2021-09-04 00:20:52 +0600

    explained function is an object

---
+++ higherOrder.js
@@ -0,0 +1,13 @@
+// higher order function
+
+// test function is a object with an example
+// example - 1
+function hello () {
+  //   console.log('Hello World')
+}
+
+hello()
+hello.firstName = 'trishan'
+// console.log(hello.firstName)
+
+// example - 2
