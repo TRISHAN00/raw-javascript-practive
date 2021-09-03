be59772f4d7d97e2c05201af6c60e5cfcbea5f1f
trishan <trishansaha43@gmail.com>
2021-09-04 00:29:46 +0600

    make function with directory console

--- higherOrder.js
+++ higherOrder.js
@@ -2,12 +2,23 @@
 
 // test function is a object with an example
 // example - 1
-function hello () {
-  //   console.log('Hello World')
-}
+// function hello () {
+//   //   console.log('Hello World')
+// }
 
-hello()
-hello.firstName = 'trishan'
+// hello()
+// hello.firstName = 'trishan'
 // console.log(hello.firstName)
 
 // example - 2
+const myfunc = function () {
+  console.log('My function')
+}
+console.dir(myfunc)
+
+// it i create a flat object let see what is look like
+const person = {
+  name: 'sakib',
+  age: '34'
+}
+console.log(person)
