4fc552eadbc3ad5ab100e0430b95243bd20e0f41
trishan <trishansaha43@gmail.com>
2021-09-03 12:09:53 +0600

    explained call and apply method

--- binding.js
+++ binding.js
@@ -21,22 +21,51 @@
 // tamim.printPlayerName()
 
 // Example - 2
-const Person = function (name, age) {
-  return {
-    name: name,
-    age: age,
-    printName: function () {
-      console.log(`the print name is ${this.name}`)
-    },
-    father: {
-      name: 'Mr, XXX',
-      printName: function () {
-        console.log(this.name)
-      }
-    }
-  }
+// const Person = function (name, age) {
+//   return {
+//     name: name,
+//     age: age,
+//     printName: function () {
+//       console.log(`the print name is ${this.name}`)
+//     },
+//     father: {
+//       name: 'Mr, XXX',
+//       printName: function () {
+//         console.log(this.name)
+//       }
+//     }
+//   }
+// }
+
+// const sakib = Person('Sakib', 35)
+// sakib.printName()
+// sakib.father.printName()
+
+// Example - 3
+const printName = function (v1, v2, v3) {
+  console.log(`${this.name} is ${v1}, ${v2} and ${v3} `)
+  //   console.log(`${this.name} is ${v} `)
 }
 
-const sakib = Person('Sakib', 35)
-sakib.printName()
-sakib.father.printName()
+const sakib = {
+  name: 'Sakib',
+  age: 35,
+  nationality: 'Bangladesh'
+}
+
+const tamim = {
+  name: 'Tamim',
+  age: 37,
+  nationality: 'Bangladesh'
+}
+
+const v1 = 'Handsome'
+const v2 = 'All-rounder'
+const v3 = 'Smart'
+
+const v = [v1, v2, v3]
+
+printName.apply(sakib, v)
+
+// printName.call(tamim, v1, v2, v3)
+// printName.call(sakib, v1, v2, v3)
