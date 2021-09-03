232381bbdd0d47ac4d403a88bf668a51a09b2733
trishan <trishansaha43@gmail.com>
2021-09-03 11:52:56 +0600

    explain double object more object in function

--- binding.js
+++ binding.js
@@ -1,21 +1,42 @@
 // Example - 1
-const printPlayerNameFunction = function (obj) {
-  obj.printPlayerName = function () {
-    console.log(this.name)
+// const printPlayerNameFunction = function (obj) {
+//   obj.printPlayerName = function () {
+//     // console.log(this.name)
+//   }
+// }
+
+// const sakib = {
+//   name: 'Sakib',
+//   age: 35
+// }
+
+// const tamim = {
+//   name: 'Tamim',
+//   age: 37
+// }
+
+// printPlayerNameFunction(sakib)
+// printPlayerNameFunction(tamim)
+// sakib.printPlayerName()
+// tamim.printPlayerName()
+
+// Example - 2
+const Person = function (name, age) {
+  return {
+    name: name,
+    age: age,
+    printName: function () {
+      console.log(`the print name is ${this.name}`)
+    },
+    father: {
+      name: 'Mr, XXX',
+      printName: function () {
+        console.log(this.name)
+      }
+    }
   }
 }
 
-const sakib = {
-  name: 'Sakib',
-  age: 35
-}
-
-const tamim = {
-  name: 'Tamim',
-  age: 37
-}
-
-printPlayerNameFunction(sakib)
-printPlayerNameFunction(tamim)
-sakib.printPlayerName()
-tamim.printPlayerName()
+const sakib = Person('Sakib', 35)
+sakib.printName()
+sakib.father.printName()
