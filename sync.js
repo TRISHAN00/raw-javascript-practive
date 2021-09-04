833c4f1a93521086f4cacd154356257a6e271832
trishan <trishansaha43@gmail.com>
2021-09-04 12:28:53 +0600

    asynchronous javascript practice with good example

--- sync.js
+++ sync.js
@@ -13,16 +13,45 @@
 // console.log('completed order for customer 1')
 
 // Asynchronous
-const processOrder = customer => {
-  console.log('process order for customer')
+// const processOrder = customer => {
+//   console.log('process order for customer')
 
-  setTimeout(() => {
-    console.log('cooking food for customer 1')
-  }, 3000)
+//   setTimeout(() => {
+//     console.log('cooking food for customer 1')
+//   }, 3000)
 
-  console.log('ready food for serve to customer 1')
+//   console.log('ready food for serve to customer 1')
+// }
+
+// console.log('take order for customer 1')
+// processOrder()
+// console.log('complete order for customer 1')
+
+// example - 3
+
+const takeOrder = (customer, callback) => {
+  console.log(`take order for ${customer}`)
+  callback(customer)
 }
 
-console.log('take order for customer 1')
-processOrder()
-console.log('complete order for customer 1')
+const processOrder = (customer, callback) => {
+  console.log(`process order for ${customer}`)
+
+  setTimeout(() => {
+    console.log(`order process for ${customer}`)
+    console.log('cooking completed')
+    callback(customer)
+  }, 3000)
+}
+
+const orderCompleted = customer => {
+  console.log(`order completed for ${customer}`)
+}
+
+takeOrder('customer 2', customer => {
+  processOrder(customer, customer => {
+    orderCompleted(customer)
+  })
+})
+
+// Example - 4
