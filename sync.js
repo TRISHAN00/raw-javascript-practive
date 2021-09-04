dba5f8dbf8630b18353ce920425f3a6f16cc29a3
trishan <trishansaha43@gmail.com>
2021-09-04 11:59:59 +0600

    sync function

---
+++ sync.js
@@ -0,0 +1,13 @@
+// Sync javascript
+// const processOrder = function (customer) {
+//   console.log('process order for customer order')
+
+//   const currentTime = new Date().getTime()
+
+//   while (currentTime + 3000 >= new Date().getTime()) {
+//     console.log('order processed for customer 1')
+//   }
+// }
+// console.log('take order from customer 1')
+// processOrder()
+// console.log('completed order for customer 1')
